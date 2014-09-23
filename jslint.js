5e62516cac4d51d25bf9049ff1d83226c5f489dd
Vanga Sasidhar <sasidhar@sasidhar.com>
2014-09-23 07:59:05 +0530

    2014.07.07

--- jslint.js
+++ jslint.js
@@ -1,5 +1,5 @@
 // jslint.js
-// 2014-07-05
+// 2014-07-07
 
 // Copyright (c) 2002 Douglas Crockford  (www.JSLint.com)
 
@@ -3043,7 +3043,7 @@ klass:              do {
             tally_property(e.string);
             break;
         }
-        if (left && (left.id === '{' || left.id === '[')) {
+        if (left && (left.id === '{' || (left.id === '[' && left.arity === 'prefix'))) {
             that.warn('unexpected_a');
         }
         step_out(']', that);
@@ -3123,6 +3123,9 @@ klass:              do {
             for (;;) {
                 edge();
                 id = identifier();
+                if (token.reserved) {
+                    token.warn('expected_identifier_a_reserved');
+                }
                 define('parameter', token);
                 parameters.push(id);
                 token.init = true;
@@ -4277,7 +4280,7 @@ klass:              do {
 
     itself.jslint = itself;
 
-    itself.edition = '2014-07-05';
+    itself.edition = '2014-07-07';
 
     return itself;
 }());
